6f05bfbc9aab2ab5ae2e5a4b3dfa023f0ca2c391
madjonr <madjonr@gmail.com>
2022-02-01 23:30:29 +0800

    mpu6050

---
+++ mpu6050/I2C_scan.py
@@ -0,0 +1,15 @@
+import machine
+sdaPIN=machine.Pin(14)
+sclPIN=machine.Pin(15)
+i2c=machine.I2C(1,sda=sdaPIN, scl=sclPIN, freq=400000)
+
+print('Scanning i2c bus')
+devices = i2c.scan()
+
+if len(devices) == 0:
+ print("No i2c device !")
+else:
+ print('i2c devices found:',len(devices))
+
+for device in devices:
+ print("Decimal address: ",device," | Hexa address: ",hex(device))
---
+++ mpu6050/picoMPU6050.py
@@ -0,0 +1,27 @@
+
+
+from imu import MPU6050
+import time
+from machine import Pin, I2C
+
+i2c = I2C(1, sda=Pin(14), scl=Pin(15), freq=400000)
+imu = MPU6050(i2c)
+
+while True:
+    # Following print shows original data get from libary. You can uncomment to see raw data
+    #print(imu.accel.xyz,imu.gyro.xyz,imu.temperature,end='\r')
+    
+    # Following rows round values get for a more pretty print:
+    ax=round(imu.accel.x,2)
+    ay=round(imu.accel.y,2)
+    az=round(imu.accel.z,2)
+    gx=round(imu.gyro.x)
+    gy=round(imu.gyro.y)
+    gz=round(imu.gyro.z)
+    tem=round(imu.temperature,2)
+#     print(ax,"\t",ay,"\t",az,"\t",gx,"\t",gy,"\t",gz,"\t",tem,"        ",end="\r")
+    print('ax:{:04}\t ay:{:04}\t az:{:04}\t gx:{:04}\t gy:{:04}\t gz:{:04}\r'.format(ax, ay, az, gx, gy, gz))
+    
+    # Following sleep statement makes values enought stable to be seen and
+    # read by a human from shell
+    time.sleep(0.2)
